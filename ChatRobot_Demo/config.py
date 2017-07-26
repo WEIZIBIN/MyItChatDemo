2e8777e7f1b7068e3f5b8d561ae42be34382acb6
WEIZIBIN <1999597@qq.com>
2017-07-26 23:14:57 +0800

    add weibo xiaoice to robot module

--- ChatRobot_Demo/config.py
+++ ChatRobot_Demo/config.py
@@ -1,6 +1,3 @@
-# set tuling robot properties here
-tuling_apiUrl = 'http://www.tuling123.com/openapi/api'
-tuling_key = '4f64796decde43f6b9e53c6b2abadb57'
 
 # set those friends you want to reply
 # using wechat remarks name
@@ -8,8 +5,15 @@ friend_wechat_remarknames = ['林育丞','蔡佳煜']
 # if replay msg from myself, you set True to test replay
 reply_msg_from_myself = True
 
-# here connect to weibo
+
+# set tuling robot properties here
+tuling_apiUrl = 'http://www.tuling123.com/openapi/api'
+tuling_key = '4f64796decde43f6b9e53c6b2abadb57'
+ROBOT_TULING = False
+
+# here connect to weibo, get xiaoice chat robot
 # properties for weibo login
 # set your weibo username and password
-WEIBO_USERNAME = 'WEIBO_USERNAME'
-WEIBO_PASSWORD = 'WEIBO_PASSWORD'
+WEIBO_USERNAME = '492760446@qq.com'
+WEIBO_PASSWORD = 'qwe544563705'
+ROBOT_XIAOICE = True
--- ChatRobot_Demo/robot.py
+++ ChatRobot_Demo/robot.py
@@ -1,8 +1,10 @@
 import requests
 import logging
-from config import tuling_apiUrl, tuling_key
+import weibo
+from config import tuling_apiUrl, tuling_key, WEIBO_USERNAME, WEIBO_PASSWORD, ROBOT_TULING, ROBOT_XIAOICE
 
 logger = logging.getLogger('MyItChatDemo.robot')
+weibo_instance = None
 
 
 def get_reply_msg(info, userid):
@@ -18,4 +20,18 @@ def get_reply_from_tuling(info, userid):
     }
     return requests.post(apiUrl, data=data).json()['text']
 
-get_reply_msg = get_reply_from_tuling
+
+def get_reply_from_xiaoice(info, userid):
+    return weibo_instance.get_msg_from_xiaoice(info)
+
+
+# set reply robot tuling
+if ROBOT_TULING:
+    get_reply_msg = get_reply_from_tuling
+
+# set reply robot xiaoice
+if ROBOT_XIAOICE:
+    weibo_instance = weibo.Weibo(WEIBO_USERNAME, WEIBO_PASSWORD)
+    weibo_instance.login()
+    weibo_instance.im_init()
+    get_reply_msg = get_reply_from_xiaoice
